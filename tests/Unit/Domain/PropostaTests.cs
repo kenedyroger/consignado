47998e79d8a0cfc7eafcaa14b410b593769cb349
Kenedy Roger <kenedyrms@gmail.com>
2025-03-28 02:28:49 -0300

    feat: (wip) Validando proposta
    Permite propostas para proponentes com menos de 80 anos

--- tests/Unit/Domain/PropostaTests.cs
+++ tests/Unit/Domain/PropostaTests.cs
@@ -12,12 +12,11 @@ public class PropostaTests
     {
         Agente agente = new Agente();
 
-        string nome = "fulano de tal";
-
         DateOnly dataAtual = DateOnly.FromDateTime(DateTime.Now);
         DateOnly dataNaoPermitida = dataAtual.AddYears(-80).AddDays(-1);
         DateOnly dataNascimento = dataNaoPermitida;
 
+        string nome = "fulano de tal";
         string numeroIdPrevidenciaSocial = "1234";
 
         string rua = "rua prof. ciclano de tal";
@@ -32,10 +31,38 @@ public class PropostaTests
         decimal valorRendimento = 0.1m;
 
         Proponente proponente = new Proponente(nome, dataNascimento, numeroIdPrevidenciaSocial, enderecoProponente, valorRendimento);
+        Proposta _sut = new Proposta(agente, proponente);
 
-        Proposta proposta = new Proposta(agente, proponente);
+        Assert.IsFalse(_sut.Validar());
+    }
 
-        Assert.IsFalse(proposta.Validar());
+    [TestMethod]
+    public void Deve_validar_se_proposta_valida_proponente_com_menos_80_anos()
+    {
+        Agente agente = new Agente();
+
+        DateOnly dataAtual = DateOnly.FromDateTime(DateTime.Now);
+        DateOnly dataNaoPermitida = dataAtual.AddYears(-80).AddDays(1);
+        DateOnly dataNascimento = dataNaoPermitida;
+
+        string nome = "fulano de tal";
+        string numeroIdPrevidenciaSocial = "1234";
+
+        string rua = "rua prof. ciclano de tal";
+        string numeroResidencial = "s/n";
+        string bairro = "parque das flores";
+        string cidade = "algum lugar";
+        string estado = "MG";
+        string cep = "00000-000";
+
+        Endereco enderecoProponente = new Endereco(rua, numeroResidencial, bairro, cidade, estado, cep);
+
+        decimal valorRendimento = 0.1m;
+
+        Proponente proponente = new Proponente(nome, dataNascimento, numeroIdPrevidenciaSocial, enderecoProponente, valorRendimento);
+        Proposta _sut = new Proposta(agente, proponente);
+
+        Assert.IsTrue(_sut.Validar());
     }
 }
 
